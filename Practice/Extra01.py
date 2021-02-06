435309c784856d66c1435f82b4091b824902e765
M A Mohit Khan <m.a.mohit.khan@gmail.com>
2021-02-06 13:18:00 +0600

    03-02-2021

---
+++ Practice/Extra01.py
@@ -0,0 +1,11 @@
+secretPass='0406'
+print('Enter the password with four digits-')
+inputPass=input()
+if inputPass==secretPass:
+  print('Access granted')
+  print('I love you')
+elif inputPass=='1234':
+  print('That is the password one idiot uses on their luggage')
+else:
+  print('Access denied')
+
---
+++ Practice/Part1/Prac1-01.py
@@ -0,0 +1,10 @@
+# This program says hello and asks for my name.
+
+print('Hello world')
+print('What is your name?') #ask name
+yourName=input()
+print('Hello there! '+yourName+' nice to you :D')
+print('The length of your name is '+str(len(yourName)))
+print('What is your age?') #ask age
+yourAge=input()
+print('You will be '+str(int(yourAge)+1)+' in a year')
